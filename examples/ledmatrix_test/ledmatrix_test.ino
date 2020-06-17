48bf8133742cdb11b3158d24ceb62e47199a82f2
Hal Rodriguez <halr66@gmail.com>
2020-06-16 18:31:22 -0700

    Replaced litterals with constants.

--- examples/ledmatrix_test/ledmatrix_test.ino
+++ examples/ledmatrix_test/ledmatrix_test.ino
@@ -1,15 +1,12 @@
 
 #include <LLedMatrix.h>
 
-void setup() 
-{
+void setup() {
 	Serial.begin(115200);
 	Serial.print("Led Matrix test.\r\n");
 	
-	while(1)
-	{
-		if(LedMatrix.check_on_line())
-		{
+	while(1) {
+		if(LedMatrix.check_on_line()) {
 			Serial.print("Led Matrix is on line.\r\n");
 			break;
 		}
@@ -18,32 +15,35 @@ void setup()
 	}
 }
 
-void loop() 
-{
+void loop() {
 	unsigned char data[5] = {0x07, 0x0e, 0x1c, 0x38, 0x70};
 
 	LedMatrix.disp_string("ABCD",4,200);
 	delay(4500);
 	LedMatrix.disp_char(' ',1);
 	delay(1000);
-	LedMatrix.disp_char('W',1);
+	LedMatrix.disp_char('X',1);
 	delay(1000);
 	LedMatrix.disp_char('a',1);
 	delay(1000);
-	LedMatrix.disp_char('y',1);
+	LedMatrix.disp_char('d',1);
 	delay(1000);
-	LedMatrix.disp_char('e',1);
+	LedMatrix.disp_char('o',1);
 	delay(1000);
-	LedMatrix.disp_char('n',1);
+	LedMatrix.disp_char('w',1);
 	delay(1000);
 
-	LedMatrix.disp_pic(0,2);
+	LedMatrix.disp_pic(DIS_PIC_JOY_LVL0,2);
 	delay(1500);
-	LedMatrix.disp_pic(1,2);
+	LedMatrix.disp_pic(DIS_PIC_JOY_LVL1,2);
 	delay(1500);
-	LedMatrix.disp_pic(2,2);
+	LedMatrix.disp_pic(DIS_PIC_SAD_LVL0,2);
 	delay(1500);
-	LedMatrix.disp_pic(3,2);
+	LedMatrix.disp_pic(DIS_PIC_SAD_LVL1,2);
+	delay(1500);
+	LedMatrix.disp_pic(DIS_PIC_SAD_LVL2,2);
+	delay(1500);
+	LedMatrix.disp_pic(DIS_PIC_SAD_LVL3,2);
 	delay(1500);
 
 	LedMatrix.disp_data(data, 2);
